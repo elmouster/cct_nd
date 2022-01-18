e9f9be301a7257abb6702363df2508ed84903635
elmouster <kebabas@gmail.com>
2022-01-18 16:35:13 +0200

    change service response to promise

--- src/radio-widget/services/radio-station-service.js
+++ src/radio-widget/services/radio-station-service.js
@@ -1,24 +1,26 @@
 export const fetchStations = (params) => {
-    return [
-        {
-            title: "labas rytas",
-            wave: 66.6,
-            pictureUrl: "ss"
-        },
-        {
-            title: "laba diena",
-            wave: 99.9,
-            pictureUrl: "aa"
-        },
-        {
-            title: "laba vakara",
-            wave: 101.1,
-            pictureUrl: "aaa"
-        },
-        {
-            title: "labanakt",
-            wave: 112.9,
-            picture: "ddd"
-        }
-    ]
+    return new Promise((resolve) => { 
+        return resolve([
+            {
+                title: "labas rytas",
+                wave: 66.6,
+                pictureUrl: "ss"
+            },
+            {
+                title: "laba diena",
+                wave: 99.9,
+                pictureUrl: "aa"
+            },
+            {
+                title: "laba vakara",
+                wave: 101.1,
+                pictureUrl: "aaa"
+            },
+            {
+                title: "labanakt",
+                wave: 112.9,
+                picture: "ddd"
+            }
+        ])
+    })
   };
